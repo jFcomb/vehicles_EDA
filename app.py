faf583aa865f4d05887336c54191c7bfd1397934
Jose Francisco Morales Breck <mbjfco@gmail.com>
2024-03-08 17:41:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly_express as px
 
 # Read csv
-df = st.dataframe(
+df = pd.read_csv(
     'C:/Users/jfcom/Documents/modulo_adicional/proyecto/vehicles_EDA-2/prep_vehicles_us.csv')
 
-st.write(df)
+st.dataframe(df)
