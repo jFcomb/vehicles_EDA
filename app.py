332ee332fac6f7246d75a410a85d5390b6d52c58
jfcomb <mbjfco@gmail.com>
2024-03-08 11:38:36 -0600

    updated

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import streamlit as st
 import plotly_express as px
+
+# Read csv
+df = pd.read_csv(
+    'C:/Users/jfcom/Documents/modulo_adicional/proyecto/vehicles_EDA/prep_vehicles_us.csv')
