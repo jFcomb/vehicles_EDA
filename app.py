69398ddd4fd88ba4e54f42de1a4a9fbae0ad11c9
jfcomb <mbjfco@gmail.com>
2024-03-11 15:53:13 -0600

    updated

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import pandas as pd
 import streamlit as st
 import plotly_express as px
 
+button = st.button('botón de prueba')
+# Title
+
+st.title("Análisis exploratorio")
+st.subheader("Tabla preprocesada")
+
 # Read csv
 df = pd.read_csv(
-    "https://github.com/jFcomb/vehicles_EDA/blob/main/prep_vehicles_us.csv?raw=true")
-# df = pd.read_csv(
-#    'C:/Users/jfcom/Documents/modulo_adicional/proyecto/vehicles_EDA/vehicles_us.csv')
+    "https://github.com/jFcomb/vehicles_EDA/blob/main/prep_vehicles_us.csv?raw=true", index_col=0)
+
 
 st.dataframe(df)
