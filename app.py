5c534c1ac039aa6d3fd2f417072e1eedbb8306a9
Jose Francisco Morales Breck <mbjfco@gmail.com>
2024-03-11 13:25:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import plotly_express as px
 
 # Read csv
 df = pd.read_csv(
-    'C:/Users/jfcom/Documents/modulo_adicional/proyecto/vehicles_EDA-2/prep_vehicles_us.csv')
+    'https://github.com/jFcomb/vehicles_EDA/blob/main/prep_vehicles_us.csv')
 
 st.dataframe(df)
