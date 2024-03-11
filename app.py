b67b9240c51d6b2951cf07be8ed88ba3410cfefd
jfcomb <mbjfco@gmail.com>
2024-03-11 14:11:47 -0600

    updated app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import streamlit as st
 import plotly_express as px
 
 # Read csv
-df = pd.read_csv('https://github.com/jFcomb/vehicles_EDA/blob/main/vehicles_us.csv')
+df = pd.read_csv(
+    "https://github.com/jFcomb/vehicles_EDA/blob/main/prep_vehicles_us.csv?raw=true")
+# df = pd.read_csv(
+#    'C:/Users/jfcom/Documents/modulo_adicional/proyecto/vehicles_EDA/vehicles_us.csv')
 
 st.dataframe(df)
